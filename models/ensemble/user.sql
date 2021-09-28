631685236372ab126bed8239eb5bcaacedaecbe2
hariprasad.srinivasan@d3analytics.com <hariprasad.srinivasan@d3analytics.com>
2021-09-28 13:35:13 +0000

    Secondary commit

--- models/ensemble/user.sql
+++ models/ensemble/user.sql
@@ -1,14 +1,9 @@
 {{config (
     materialized = 'table')
 }}
-
-with app as (
-
     select
         AppName,
         count(distinct(ActiveUserId)) as number_of_users
-
-    from Activ
-
+    from ENSEMBLE.ACTIV
     group by AppName
- )
+ 
